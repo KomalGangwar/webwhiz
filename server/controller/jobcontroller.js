58dafa6402aa1630a0261ef59c979ce321b619b7
Komal  Gangwar <107317696+KomalGangwar@users.noreply.github.com>
2024-05-28 13:05:21 +0530

    Add files via upload

---
+++ server/controller/jobcontroller.js
@@ -0,0 +1,106 @@
+import db from "../database/conn.js";
+
+export const addJob = async (req, res) => {
+    try {
+        // console.log(req.body);
+        const sql = `INSERT INTO job(Job_Name,Job_Desc,Job_salary,Com_Name ) VALUES ("${req.body.name}", "${req.body.desc}" , ${Number(req.body.salary)},"${req.body.cname}")`;
+        db.query(sql,(error,result) => {
+            if(error)
+               return res.status(422).json(error);
+            res.status(200).json(result);
+        })
+    } catch (error) {
+        res.status(500).json({message:"Server error",error})
+    }
+}
+
+export const getAllJob = async(req,res) => {
+    try {
+        const sql = "Select * from job";
+        db.query(sql,(error,result)=>{
+            if(error)
+                return res.status(422).json(error);
+            res.status(200).json(result);
+        })
+    } catch (error) {
+        res.status(500).json({message : "Server error", error})
+    }
+}
+
+export const getJob = async (req , res) => {
+    try {
+        const sql = `select * from job where Job_id="${req.params.id}"`;
+        db.query(sql,(error,result)=>{
+            if(error)
+                return res.status(422).json(error);
+            res.status(200).json(result);
+        })
+    } catch (error) {
+        res.status(500).json({message : "Server error", error})
+    }
+}
+
+export const updateJob = async(req,res) => {
+    try{
+        const sql = `Update job set Job_Desc = "${req.body.Job_Desc}" , Job_Name = "${req.body.Job_Name}" ,Job_salary = ${req.body.Job_salary}, Com_Name= "${req.body.Com_Name}" where Job_id= "${req.body.Job_id}" `;
+        db.query(sql,(error,result)=>{
+            if(error)
+                return res.status(422).json(error);
+            res.status(200).json(result);
+        })
+    } catch (error) {
+        res.status(500).json({message : "Server error", error});
+    }
+}
+
+export const deleteJob = async(req,res) => {
+    try{
+        const sql = `Delete from job where Job_id = "${req.body.id}" `;
+        db.query(sql,(error,result)=>{
+            if(error)
+                return res.status(422).json(error);
+            res.status(200).json(result);
+        })
+    } catch (error) {
+        res.status(500).json({message : "Server error", error});
+    }
+}
+
+export const applyJob = async (req , res) => {
+    try {
+        const sql = `INSERT INTO jobapply (Enrol_num,Sname,Job_id,Job_Name,Com_Name) VALUES("${req.body.sId}" ,"${req.body.sName}" ,${req.body.jId},"${req.body.jName}" ,"${req.body.cName}" );`;
+        db.query(sql,(error,result)=>{
+            if(error)
+                return res.status(422).json(error);
+            res.status(200).json(result);
+        })
+    } catch (error) {
+        res.status(500).json({message : "Server error", error});
+    }
+}
+
+export const viewApplyJob = async (req , res) => {
+    try {
+        const sql = `Select Job_id,Job_Name,Com_Name from jobapply WHERE Enrol_num="${req.params.id}"`;
+        db.query(sql,(error,result)=>{
+            if(error)
+                return res.status(422).json(error);
+            res.status(200).json(result);
+        })
+    } catch (error) {
+        res.status(500).json({message : "Server error", error});
+    }
+}
+
+export const getAllAppliedJob = async (req , res) => {
+    try {
+        const sql = `Select Enrol_num,Sname,Job_Name,Com_Name from jobapply`;
+        db.query(sql,(error,result)=>{
+            if(error)
+                return res.status(422).json(error);
+            res.status(200).json(result);
+        })
+    } catch (error) {
+        res.status(500).json({message : "Server error", error});
+    }
+}
---
+++ server/route/routes.js
@@ -0,0 +1,44 @@
+import express,{Router} from "express";
+import { getAllStudent, addStudent, getStudent , updateStudent, deleteStudent} from "../controller/studentcontroller.js";
+import { addCompany, deleteCompany, getAllCompany, updateCompany, getCompany } from "../controller/companycontroller.js";
+import { addJob, getAllJob, deleteJob, updateJob, getJob, applyJob, viewApplyJob, getAllAppliedJob } from "../controller/jobcontroller.js";
+import { updateAdmin , get_Admin } from "../controller/admincontroller.js";
+const router = express.Router();
+
+router.get("/",(req,res)=>{
+    res.send("Hey man");
+})
+
+//////////////////////////////////////////////////////////////////////
+//Admin
+router.post("/loginadmin",getAdmin);
+router.post("/adminupdate",updateAdmin);
+
+//////////////////////////////////////////////////////////////////////
+//Student
+router.post("/addstudent",addStudent);
+router.get("/getallstudent",getAllStudent);
+router.post("/updatestudent",updateStudent);
+router.post("/getstudent",getStudent);
+router.post("/deletestudent", deleteStudent);
+
+//////////////////////////////////////////////////////////////////////
+//Company
+router.post("/addcompany",addCompany);
+router.get("/getallcompany", getAllCompany);
+router.get("/getcompany/:id",getCompany);
+router.post("/updatecompany", updateCompany);
+router.post("/deletecompany", deleteCompany);
+
+/////////////////////////////////////////////////////////////////////
+//Job
+router.post("/addjob", addJob);
+router.get("/getalljob", getAllJob);
+router.get("/getjob/:id", getJob);
+router.post("/deletejob", deleteJob);
+router.post("/updatejob", updateJob);
+router.post("/applyjob", applyJob);
+router.get("/viewapplyjob/:id", viewApplyJob); //student
+router.get("/getallappliedjob", getAllAppliedJob); //admin
+
+export default router;
